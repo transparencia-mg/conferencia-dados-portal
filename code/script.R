4d48f7af5fe0ecc75f19d27e76491e9d75a7172b
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2020-02-21 11:57:29 -0300

    Adiciona uso do pacote dplyr para manipulacao dos dados

--- code/script.R
+++ code/script.R
@@ -10,4 +10,26 @@ tab <- html_table(pg, fill=TRUE)[[1]]
 tab_names <- names(tab)
 names(tab) <- make_clean_names(tab_names)
 
-tab <- mutate(tab, total = num_converte(total))
+tab <- mutate(tab, ano = 2019,
+              valor_pessoal_ativo = num_converte(valor_pessoal_ativo),
+              valor_pessoal_inativo = num_converte(valor_pessoal_inativo),
+              valor_pensionistas = num_converte(valor_pensionistas),
+              valor_contratados = num_converte(valor_contratados),
+              total = num_converte(total))
+
+
+tab_sem_total <- filter(tab, orgao != "TOTAL GERAL:")
+tab_sem_total_total <- select(tab_sem_total, total)
+summarize(tab_sem_total_total, n = sum(total))
+
+tab %>% 
+  filter(orgao != "TOTAL GERAL:") %>% 
+  select(total) %>% 
+  summarize(n = sum(total))
+
+
+filter(tab, orgao == "TOTAL GERAL:")
+
+
+
+# testar se soma das colunas é igual ao total
